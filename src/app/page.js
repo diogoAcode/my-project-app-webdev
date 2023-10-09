c17891ab6bc8c845ba1b66a56864b35731692430
Diogo Almeida <diogoalmeida.code@gmail.com>
2023-10-09 16:37:42 -0300

    Creates Appbar and Bottom

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
-import Image from 'next/image'
+import Appbar from "./components/Appbar";
+import Bottom from "./components/Bottom";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">        
-      </div>
+    <main className="min-h-screen">
+      <Appbar></Appbar>
+
+      <Bottom></Bottom>
     </main>
-  )
+  );
 }
